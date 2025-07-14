4869f8f43cdd9440503b2d45ac571a16bc70bce3
arsen00531 <arsendag505@gmail.com>
2025-07-15 00:33:41 +0300

    add robots seo

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://fulfilment-moskov.ru/',
+  generateRobotsTxt: true,
+  changefreq: 'monthly',
+  priority: 0.7,
+};
