270764e4819f91bd1025f62a7dc03cda65195672
arsen00531 <arsendag505@gmail.com>
2025-07-16 11:36:22 +0300

    add yandex counter

--- app/layout.tsx
+++ app/layout.tsx
@@ -82,8 +82,32 @@ export default function RootLayout({
             gtag('config', 'G-0E5T9PLRD7');
           `}
         </Script>
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(103349890, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });
+          `}
+        </Script>
+        {/* /Yandex.Metrika counter */}
       </head>
       <body className={`${fontSans.variable} ${gilroy.variable} font-gilroy`}>
+        {/* Yandex.Metrika noscript */}
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/103349890" style={{position:"absolute", left:"-9999px"}} alt="" />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika noscript */}
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <Header />
           <main>{children}</main>
